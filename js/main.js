901f4e9016fe4e9f04e5b48f2319e995308515ff
Screamo95 <bykovglobal@gmail.com>
2024-04-25 16:32:23 +0300

    Разработка бургер меню, галереи и модальных окон

--- js/main.js
+++ js/main.js
@@ -49,3 +49,14 @@ document.querySelector(`#question__item-top-4`).onclick = function() {
   document.querySelector(`#question__item-img-2`).src = "img/question_arrow.svg";
   document.querySelector(`#question__item-img-3`).src = "img/question_arrow.svg";
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var media = document.querySelectorAll('.materialboxed');
+  var instances_media = M.Materialbox.init(media);
+
+  var modals = document.querySelectorAll('.modal');
+  var instances_modals = M.Modal.init(modals);
+
+  var sidenav = document.querySelectorAll('.sidenav');
+  var instances_sidenav = M.Sidenav.init(sidenav);
+});
